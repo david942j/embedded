b14f4d5a63a45e902bd2673ee5bda9a91317dc1d
david942j <david942j@gmail.com>
2014-11-13 00:30:48 +0800

    layout

--- application/views/header.php
+++ application/views/header.php
@@ -3,8 +3,7 @@
 <title>嵌入式系统2014</title>
 <body style="background-color:#ffffff">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
-<?=link_tag(css_url().'bootstrap.min2.3.2.css');?>
+<?=link_tag(css_url().'bootstrap2.3.2min.css');?>
 <?=link_tag(css_url().'layout.css');?>
 <?=link_tag(css_url().'login.css');?>
 <?=link_tag(css_url().'map/user/user.css');?>
@@ -14,4 +13,48 @@
 <script src="<?= js_url() ?>map.js"></script>
 <script src="<?= js_url() ?>cards.js"></script>
 <script src="<?= js_url() ?>cookie.js"></script>
+<? if(isset($user)) {?>
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="brand" href="<?= base_url() ?>">BeagleBoard</a>
+			<div class="nav-collapse collapse">
+				<ul class="nav">
+					<li class="">
+						<a href="<?= base_url() ?>">Home</a>
+					</li>
+					<li class="dropdown">
+						<a id="drop1" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">網路設定 <b class="caret"></b></a>
+						<ul class="dropdown-menu" role="menu" aria-labelledby="drop1">
+							<li role="presentation"><a role="menuitem" tabindex="-1" href="<?= base_url() ?>click/">有線網路</a></li>
+							<li role="presentation"><a role="menuitem" tabindex="-1" href="<?= base_url() ?>game/lottery">無線網路</a></li>
+							<li role="presentation" class="divider"></li>
+							<li role="presentation"><a role="menuitem" target="_blank" tabindex="-1" href="http://www.youtube.com/watch?v=vieVk_n2ECQ">喵喵叫</a></li>
+						</ul>
+					</li>
+					<li class="">
+						<a href="<?= base_url() ?>quests">Quests</a>
+					</li>
+					<li class="">
+						<a href="<?= base_url() ?>about_eenight">About EEnight</a>
+					</li>
+					<li class="">
+						<a href="#main_rule_modal" data-toggle="modal">Rules</a>
+					</li>
+				</ul>
+				<ul class="nav pull-right">
+					<li class="">
+						<a><?= $user?></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+<? } ?>
 <div class='layout'>
--- application/views/index.php
+++ application/views/index.php
@@ -1,5 +1,3 @@
 <?php include 'header.php'; ?>
-<?php
-print $user;
-?>
+
 <?php include 'footer.php'; ?>
